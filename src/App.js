dd0efdef305c94c5077fa456992a51eff655ecce
Amaro Darchez <amarodarchez@gmail.com>
2022-05-21 13:23:22 -0300

    Bootstrap library added.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Listado from "./components/Listado";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
+import "./css/bootstrap.min.css";
+
 function App() {
   return (
     <>
