1acf152f401a93cbede854a2a23ab09e36539ebb
Amaro Darchez <amarodarchez@gmail.com>
2022-05-24 16:55:08 -0300

    movies database API called from Listado component

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,8 +1,20 @@
 import { Navigate } from 'react-router-dom';
-
-const token = localStorage.getItem('token');
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Listado() {
+
+    const token = localStorage.getItem('token');
+    const [ moviesList, setMoviesList ] = useState([]);
+
+    useEffect(() => {
+        const endPoint = 'https://api.themoviedb.org/3/discover/movie?api_key=1aeabb44f43a9dd9dda9d0fdbf53257e';
+        axios.get(endPoint)
+            .then(response => {
+                setMoviesList(response.data.results);
+            })
+    }, []);
+
     return (
         <>
             {!token && <Navigate to='/' />}
