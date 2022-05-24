418e9a9f3841dbef095259669b72f133a6cbb195
Amaro Darchez <amarodarchez@gmail.com>
2022-05-24 18:12:45 -0300

    API response. List of movies rendered in Listado component

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -1,34 +1,37 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Listado() {
 
     const token = localStorage.getItem('token');
-    const [ moviesList, setMoviesList ] = useState([]);
+    const [moviesList, setMoviesList] = useState([]);
 
     useEffect(() => {
         const endPoint = 'https://api.themoviedb.org/3/discover/movie?api_key=1aeabb44f43a9dd9dda9d0fdbf53257e';
         axios.get(endPoint)
             .then(response => {
-                setMoviesList(response.data.results);
+                let data = response.data.results;
+                setMoviesList(data);
             })
-    }, []);
+    },[]);
 
     return (
         <>
             {!token && <Navigate to='/' />}
             <div className="row">
-                <div className="col-3">
-                    <div className="card">
-                        <img src="..." className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                {moviesList.map((movie) => (
+                    <div className="col-3 my-4" key={movie.id}>
+                        <div className="card">
+                            <img src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`} className="card-img-top" alt="..." />
+                            <div className="card-body">
+                                <h5 className="card-title">{movie.title}</h5>
+                                <p className="card-text">{movie.overview}</p>
+                                <Link to="/" className="btn btn-primary">Go somewhere</Link>
+                            </div>
                         </div>
                     </div>
-                </div>
+                ))}
             </div >
         </>
     )
